c91dfe07e23a28848be85d1ef5fe22d387d6f02d
BsongJH <Bsong9916@gmail.com>
2023-12-20 16:37:53 -0800

    change of plans

---
+++ src/FileOperation.java
@@ -0,0 +1,16 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public abstract class FileOperation
+{
+    protected Workbook workbook;
+    protected Sheet sheet;
+    protected Row headerRow;
+    protected Cell cell;
+
+    // TODO
+    public String getRow()
+    {
+
+    }
+}
--- src/Write.java
+++ src/Write.java
@@ -1,9 +1,10 @@
 import java.io.FileWriter;
 
-public class Write extends FileOp
+public class Write extends FileOperation
 {
     // TODO Find a way to save on spreadsheet with Class Computer and Loan
     // Have the function write separate unavailable/available (?)
+    // Merge Write and Delete and search?
     public Write()
     {
 
